c6526543d1f652ca28b6fbbd49a7f8b974e6a709
Michal <krzyzowskim1998@gmail.com>
2024-05-09 20:05:39 +0200

    CHORE: Changed system prompt for ollama translation task to increase efficiency (it helped a bit)

--- services/translations/OllamaTranslationService.py
+++ services/translations/OllamaTranslationService.py
@@ -8,9 +8,12 @@ class OllamaTranslationService(TranslationService):
     self.client = client or OllamaService()
 
   def translate(self, text: str) -> str:
-    system_prompt = """
-      I get text to translate, from english to polish or from polish to english depend on content.
-      I have to return only tranlsation! I don't add any additional description/ information, just pure translation.
+    system_prompt = f"""
+      Your task is to translate text, from english to polish or from polish to english depend on content.
+      You have to return only tranlsation nothing more! You don't add any additional description/ information, just pure translation.
+      ###
+      TEXT TO TRANSLATE: {text}
     """
 
-    return self.client.process_request(system_prompt, text)
+    return self.client.process_request('', system_prompt)
+
