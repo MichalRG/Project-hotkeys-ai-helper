b8cbe028d4317d8bb74516fc7b06ffd2957bfcff
Michal <krzyzowskim1998@gmail.com>
2024-05-14 19:53:58 +0200

    FEAT: added main.py to handle new general question task

--- main.py
+++ main.py
@@ -6,5 +6,7 @@ event_manager = EventManager()
 
 keyboard.add_hotkey('ctrl+alt+1', event_manager.translate_event)
 keyboard.add_hotkey('ctrl+alt+2', event_manager.process_wolfram_action)
+keyboard.add_hotkey('ctrl+alt+4', event_manager.process_general_question)
 
 keyboard.wait('ctrl+alt+num 0') 
+
